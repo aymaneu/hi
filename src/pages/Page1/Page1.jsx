d367d6f1ee856ef473973553024420941a4af003
elgad aymane <aymanelgad95@gmail.com>
2022-05-06 11:27:29 +0000

    first commit

---
+++ src/pages/Page1/Page1.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Page1 = () => {
+  return (
+    <div className="Box">
+      <div className="Title">
+        <h1>Please chose the Company</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Page1;
